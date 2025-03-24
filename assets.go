37eca2f9d28d7a0bcf17cd949ec2983efc8bef50
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-03-23 21:26:08 -0400

    Chp 5 complete

--- assets.go
+++ assets.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -9,4 +18,81 @@ func (cfg apiConfig) ensureAssetsDir() error {
 		return os.Mkdir(cfg.assetsRoot, 0755)
 	}
 	return nil
+}
+
+func getAssetPath(mediaType string) string {
+	base := make([]byte, 32)
+	_, err := rand.Read(base)
+	if err != nil {
+		panic("failed to generate random bytes")
+	}
+	id := base64.RawURLEncoding.EncodeToString(base)
+
+	ext := mediaTypeToExt(mediaType)
+	return fmt.Sprintf("%s%s", id, ext)
+}
+
+func (cfg apiConfig) getObjectURL(key string) string {
+	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
+}
+
+func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
+	return filepath.Join(cfg.assetsRoot, assetPath)
+}
+
+func (cfg apiConfig) getAssetURL(assetPath string) string {
+	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetPath)
+}
+
+func mediaTypeToExt(mediaType string) string {
+	parts := strings.Split(mediaType, "/")
+	if len(parts) != 2 {
+		return ".bin"
+	}
+	return "." + parts[1]
+}
+
+func getVideoAspectRatio(filepath string) (string, error) {
+	var cmdBuffer bytes.Buffer
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filepath)
+	cmd.Stdout = &cmdBuffer
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	var probeData struct {
+		Streams []struct {
+			Width  int `json:"width"`
+			Height int `json:"height"`
+		} `json:"streams"`
+	}
+	err = json.Unmarshal(cmdBuffer.Bytes(), &probeData)
+	if err != nil {
+		return "", err
+	}
+
+	if len(probeData.Streams) == 0 {
+		return "", errors.New("no streams found")
+	}
+
+	width := probeData.Streams[0].Width
+	height := probeData.Streams[0].Height
+
+	if width > height && float32(width)/float32(height) >= 1.7 && float32(width)/float32(height) <= 1.8 {
+		return "16:9", nil
+	} else if height > width && float32(height)/float32(width) >= 1.7 && float32(height)/float32(width) <= 1.8 {
+		return "9:16", nil
+	} else {
+		return "other", nil
+	}
+}
+
+func processVideoForFastStart(filepath string) (string, error) {
+	outputPath := filepath + ".processing"
+	cmd := exec.Command("ffmpeg", "-i", filepath, "-c", "copy", "-movflags", "faststart", "-f", "mp4", outputPath)
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	return outputPath, nil
 }
